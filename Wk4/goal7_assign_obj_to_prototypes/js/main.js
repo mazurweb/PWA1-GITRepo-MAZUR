5504bdb1ed25c1f05205f24c03059fd9cb65d3ff
mazurweb <mazurweb@gmail.com>
2015-05-29 14:11:09 -0400

    push

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -1,6 +1,21 @@
 /**
  * Created by the JavaScript Development Team
+ * Michael Mazur
+ * Date: 5-29-2015
  * Class: PWA
  * Goal: Goal7
  */
 
+//creation of var names = ["Mike", "Bob", "Sue", "Amber", "Rick"] stores 5 names
+
+
+
+//FOR(var i = 0; i<3; i++)  Runs through the loop 3 times
+//set variable for someName = Math.floor(Math.random() * 5) +1)
+//set variable for someRow = i +1
+//Instantiate Person object var person = New Person(someName, someRow)
+//END OF FOR LOOP
+
+//function populateHTML(){
+//
+//}
