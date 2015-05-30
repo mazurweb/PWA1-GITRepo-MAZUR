e3bf7b29348f2b187457af8e809a776abaddd09a
mazurweb <mazurweb@gmail.com>
2015-05-29 21:29:27 -0400

    push

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -7,15 +7,40 @@
  */
 
 //creation of var names = ["Mike", "Bob", "Sue", "Amber", "Rick"] stores 5 names
-
-
+//var people = [];
+//var r1c1 = document.querySelector('#r1c1'),
+//    r1c2 = document.querySelector('#r1c2'),
+//    r1c3 = document.querySelector('#r1c3'),
+//    r2c1 = document.querySelector('#r2c1'),
+//    r2c2 = document.querySelector('#r2c2'),
+//    r2c3 = document.querySelector('#r2c3'),
+//    r3c1 = document.querySelector('#r3c1'),
+//    r3c2 = document.querySelector('#r3c2'),
+//    r3c3 = document.querySelector('#r3c3');
 
 //FOR(var i = 0; i<3; i++)  Runs through the loop 3 times
 //set variable for someName = Math.floor(Math.random() * 5) +1)
 //set variable for someRow = i +1
-//Instantiate Person object var person = New Person(someName, someRow)
+//Instantiate Person object people = New Person(someName, someRow)
 //END OF FOR LOOP
 
 //function populateHTML(){
-//
+//IF (people[0].row === 1)
+//THEN
+//   r1c1.innerHTML = people[0].name;
+//   r1c2.innerHTML = people[0].jobs;
+//   r1c3.innerHTML = people[0].action;
+//ELSEIF (people[1].row === 2)
+//THEN
+//   r2c1.innerHTML = people[1].name;
+//   r2c2.innerHTML = people[1].job;
+//   r2c3.innerHTML = people[1].action;
+//ELSE
+//   r3c1.innerHTML = people[2].name;
+//   r3c2.innerHTML = people[2].jobs;
+//   r3c3.innerHTML = people[2].actions;
 //}
+
+
+
+//setInterval(runUpdate(), 1000/30);
--- Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -3,3 +3,13 @@
  * Class: PWA
  * Goal: Goal7
  */
+
+//globally add Person Object, window.Person = Person;
+
+//person object constructor storing this.name this.job this.action
+
+//Person.jobs = ["teacher", "farmer","student", "pilot"];
+//Person.actions = ["sleeping", "working", "traveling"];
+
+//Didn't get to finish the assignment.  Was at Disney over the weekend and fell behind the last week.
+
